15c97571f17cfb3d9f2b137d265be32ea40316e7
Alonso <alons182@hotmail.com>
2015-08-27 13:55:13 -0600

    menu fix

--- wp-content/themes/municipalliberia/assets/js/main.js
+++ wp-content/themes/municipalliberia/assets/js/main.js
@@ -28,7 +28,7 @@
 
     menu.find(".menu-item").hover(
           function() {
-                menu.find(".menu-item").css('opacity','.5');
+                menu.find(".menu-item").css('opacity','0.5');
                 $(this).css('opacity','1');
 
               },

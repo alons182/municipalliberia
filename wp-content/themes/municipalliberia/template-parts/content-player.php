8c195cabaea44b4755c9f9f79b516e2e2925dd8b
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-08-08 09:29:54 -0600

    se agrego thumb de foto en el detalle de jugador

--- wp-content/themes/municipalliberia/template-parts/content-player.php
+++ wp-content/themes/municipalliberia/template-parts/content-player.php
@@ -28,42 +28,65 @@
 			
 			<div>
 				
-				<div class="player-positions">
-					<h4>Posiciones</h4>
+				<div class="player-right">
+						<div class="player-positions">
+						<h4>Posiciones</h4>
 
-					<?php 
-						$pos = rwmb_meta( 'rw_player_position');
-						$pos2 = rwmb_meta( 'rw_player_position_2');
-						
-						function create_slug($string){
-						   $slug=preg_replace('/[^A-Za-z0-9-]+/', '-', $string);
-						   return $slug;
-						}
-						
-					   ?>
-					   <img src="<?php echo get_template_directory_uri();?>/img/pos_main.png" alt="<?php echo $pos; ?>" class="<?php echo create_slug($pos); ?>" title="<?php echo $pos; ?>" />
-					   <?php if($pos == 'Defensa Central') : ?>
-					   		<img src="<?php echo get_template_directory_uri();?>/img/pos_main.png" alt="<?php echo $pos; ?>" class="<?php echo create_slug($pos.'2'); ?>" title="<?php echo $pos; ?>" />
-					   	<?php endif ?>
-					   	
-						<?php if($pos2) : ?>
-							<img src="<?php echo get_template_directory_uri();?>/img/pos_secondary.png" alt="<?php echo $pos2; ?>" class="<?php echo create_slug($pos2); ?> secondary" title="<?php echo $pos2; ?>" />
-							<?php if($pos2 == 'Defensa Central') : ?>
-					   			<img src="<?php echo get_template_directory_uri();?>/img/pos_secondary.png" alt="<?php echo $pos2; ?>" class="<?php echo create_slug($pos2.'2'); ?> secondary" title="<?php echo $pos2; ?>" />
-					   		<?php endif ?>
-					   		
+						<?php 
+							$pos = rwmb_meta( 'rw_player_position');
+							$pos2 = rwmb_meta( 'rw_player_position_2');
+							
+							function create_slug($string){
+							   $slug=preg_replace('/[^A-Za-z0-9-]+/', '-', $string);
+							   return $slug;
+							}
+							
+						   ?>
+						   <img src="<?php echo get_template_directory_uri();?>/img/pos_main.png" alt="<?php echo $pos; ?>" class="<?php echo create_slug($pos); ?>" title="<?php echo $pos; ?>" />
+						   <?php if($pos == 'Defensa Central') : ?>
+						   		<img src="<?php echo get_template_directory_uri();?>/img/pos_main.png" alt="<?php echo $pos; ?>" class="<?php echo create_slug($pos.'2'); ?>" title="<?php echo $pos; ?>" />
+						   	<?php endif ?>
+						   	
+							<?php if($pos2) : ?>
+								<img src="<?php echo get_template_directory_uri();?>/img/pos_secondary.png" alt="<?php echo $pos2; ?>" class="<?php echo create_slug($pos2); ?> secondary" title="<?php echo $pos2; ?>" />
+								<?php if($pos2 == 'Defensa Central') : ?>
+						   			<img src="<?php echo get_template_directory_uri();?>/img/pos_secondary.png" alt="<?php echo $pos2; ?>" class="<?php echo create_slug($pos2.'2'); ?> secondary" title="<?php echo $pos2; ?>" />
+						   		<?php endif ?>
+						   		
 
-					   			
-						<?php endif ?>
-						<div class="player-positions-titles">
-							<div class="player-positions-titles-item">Pos. Principal: <?php echo $pos; ?></div>
-						<?php if($pos2) : ?>
-							<div class="player-positions-titles-item">Pos. Secundaria: <?php echo $pos2; ?></div>
-						<?php endif ?>
-						</div>
-						
+						   			
+							<?php endif ?>
+							<div class="player-positions-titles">
+								<div class="player-positions-titles-item">Pos. 1: <?php echo $pos; ?></div>
+							<?php if($pos2) : ?>
+								<div class="player-positions-titles-item">Pos. 2: <?php echo $pos2; ?></div>
+							<?php endif ?>
+							</div>
+							
+							
 
+					</div>
+					<div class="player-img">
+								<?php $images = rwmb_meta( 'rw_player_thumb', 'type=image&size=large' ); 
+						         if ( $images ) {?>
+						         
+						         
+						            
+						                 <?php foreach ( $images as $image ){?>
+						                     
+						                     <img src="<?php echo $image['url'] ?>" alt="<?php the_title(); ?>" />
+						                  
+						                  <?php } ?>
+
+						                 
+						            
+
+						         <?php         
+						              }
+						          ?>
+							</div>
 				</div>
+				
 				<?php the_content(); ?>
 			</div>
 			

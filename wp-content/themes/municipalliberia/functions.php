078e483ab37d51ec64da53994bd3e27d040a11d9
Alonso Chavarria Cubero <alons182@hotmail.com>
2016-09-19 17:54:03 -0600

    se se carga navigation.js

--- wp-content/themes/municipalliberia/functions.php
+++ wp-content/themes/municipalliberia/functions.php
@@ -130,7 +130,7 @@ add_action( 'widgets_init', 'municipalliberia_widgets_init' );
 function municipalliberia_scripts() {
 	wp_enqueue_style( 'municipalliberia-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'municipalliberia-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	//wp_enqueue_script( 'municipalliberia-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'municipalliberia-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
